acdfb106c5e8ed6947a1edb7a3cb02a816d2b1ce
Fernanda Felix <fe.santosfj09@gmail.com>
2024-10-03 13:57:02 -0300

    adicionando algumas funções javascript

---
+++ Front/html/script.js
@@ -0,0 +1,22 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+//abrir carrinho
+cartBtn.addEventListener("click", function(){
+    cartModal.style.display = "flex"
+})
+
+//fechar modal clicando na tela
+cartModal.addEventListener("click", function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
