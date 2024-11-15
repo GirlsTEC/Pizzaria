473f7248a9bfbb8440f9df74295a3436e19f60e5
Cleiciane Silva <92116369+cleiciane55@users.noreply.github.com>
2024-11-15 20:28:12 -0300

    Adicionando validação a criação de usuário

--- public/js/scriptCadastro.js
+++ public/js/scriptCadastro.js
@@ -21,17 +21,22 @@ formCadastro.addEventListener('submit', (event) => {
     event.preventDefault()
     const data = Object.fromEntries(new FormData(event.target).entries());
 
-    fetch('http://localhost:8080/api/cliente/create', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    }).then(res => {
-        if (res.ok) {
-            location.replace('/login');
-        }
-        return res;
-    }).then(data => console.log(data))
-        .catch(err => console.log("ERROR: " + err));
+    if (data.senha === data.confirmaSenha && data.senha !== "") {
+        fetch('http://localhost:8080/api/cliente/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        }).then(res => {
+            if (res.ok) {
+                alert('Cadastrado com sucesso!');
+                location.replace('/login');
+            }
+            return res.json();
+        }).then(data => console.log(data.message))
+            .catch(err => console.log("ERROR: " + err));
+    } else {
+        alert('As senhas não coincidem ou estão vazias.');
+    }
 });
