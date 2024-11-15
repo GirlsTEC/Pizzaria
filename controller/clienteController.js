0e48bdb2c0e2652514c0ad918967e211cf4c4145
Cleiciane Silva <92116369+cleiciane55@users.noreply.github.com>
2024-11-15 20:44:52 -0300

    Padronizando o tipo de aspas e o retorno das respostas para JSON

--- controller/clienteController.js
+++ controller/clienteController.js
@@ -4,10 +4,10 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const getAllClientes = (req, res) => {
-    // pega todos os clientes no bdd
+    // Pega todos os clientes no bdd
     pool.query(queries.getAllClientes, (error, results) => {
         if (error) throw error;
-        console.log("Pegou todos os Clientes")
+        console.log('Pegou todos os Clientes')
         return res.status(200).json(results.rows);
     })
 };
@@ -16,9 +16,9 @@ const getClienteById = (req, res) => {
     let id = parseInt(req.params.id);
 
     pool.query(queries.getClienteById, [id], (error, results) => {
-        if (error) return res.status(500).send("Erro ao achar o Cliente");
-        if (!results.rows.length) return res.status(404).send('Cliente não encontrado');
-        console.log("Pegou cliente do id " + id)
+        if (error) return res.status(500).send({message: 'Erro ao achar o Cliente'});
+        if (!results.rows.length) return res.status(404).send({message: 'Cliente não encontrado'});
+        console.log('Pegou cliente do id ' + id)
         return res.status(200).json(results.rows);
     })
 };
@@ -29,7 +29,7 @@ const getClienteByToken = (req, res) => {
     pool.query(queries.getClienteByToken, [refreshToken], (error, results) => {
         if (error) return res.status(500).send({message: 'Erro ao achar o Cliente'});
         if (!results.rows.length) return res.status(404).send({message: 'Cliente não encontrado'});
-        console.log("Pegou cliente");
+        console.log('Pegou cliente');
         const user = {
             nome: results.rows[0].nome,
             email: results.rows[0].email,
@@ -44,12 +44,12 @@ const createCliente = (req, res) => {
     const {nome, senha, email, telefone, endereco} = req.body;
 
     if (!nome || !senha || !email || !telefone || !endereco) {
-        return res.status(400).send("Preencha Todos os Campos!")
+        return res.status(400).send({message: 'Preencha Todos os Campos!'})
     }
     // checa a já existência do $email
     pool.query(queries.checkEmailExiste, [email], (error, results) => {
         if (results.rows.length) {
-            return res.status(400).send("O E-mail já existe");
+            return res.status(400).send({message: 'O E-mail já existe'});
         }
 
         //Hash Password
@@ -58,10 +58,10 @@ const createCliente = (req, res) => {
         // cria clientes no db
         pool.query(queries.addCliente, [nome, email, hashedSenha, telefone, endereco], (error, results) => {
             if (error) {
-                console.error("Erro ao adicionar cliente:", error);
-                return res.status(500).send("Erro ao adicionar cliente");
+                console.error('Erro ao adicionar cliente:', error);
+                return res.status(500).send({message: 'Erro ao adicionar cliente'});
             }
-            return res.status(201).send("Cliente Criado com Sucesso!");
+            return res.status(201).send({message: 'Cliente Criado com Sucesso!'});
         });
     })
 };
@@ -69,7 +69,7 @@ const createCliente = (req, res) => {
 const updateCliente = (req, res) => {
     const { nome, senha, telefone, endereco } = req.body;
     const { email } = req.user;
-    let query = "UPDATE Cliente SET";
+    let query = 'UPDATE Cliente SET';
 
     pool.query(queries.getClienteByEmail, [email], (error, results) => {
         if (error) return res.status(500).send({message: 'Erro ao achar o Cliente'});
@@ -77,24 +77,24 @@ const updateCliente = (req, res) => {
         let count = 0
 
         const user = results.rows[0];
-        if (nome !== user.nome && nome !== "") {
+        if (nome !== user.nome && nome !== '') {
             ++count;
             query = query.concat(" nome = '", nome, "',");
         }
-        if (senha !== user.senha && senha !== "") {
+        if (senha !== user.senha && senha !== '') {
             ++count;
             query = query.concat(" senha = '", senha, "',");
         }
-        if (telefone !== user.telefone && telefone !== "") {
+        if (telefone !== user.telefone && telefone !== '') {
             ++count;
-            query = query.concat(" telefone = '", telefone, "',");
+            query = query.concat(" telefone = '", telefone,"',");
         }
-        if (endereco !== user.endereco && endereco !== "") {
+        if (endereco !== user.endereco && endereco !== '') {
             ++count;
             query = query.concat(" endereco = '", endereco, "'");
         }
         if(query.endsWith(',')) query = query.slice(0, -1);
-        query = query.concat(" WHERE email = '", email, "';");
+        query = query.concat("WHERE email = '", email, "';");
         console.log(query);
         console.log(queries.getClienteByEmail);
         if (!count) return res.status(400).send({message: 'Não há necessidade de atualizar'});
@@ -109,14 +109,14 @@ const updateCliente = (req, res) => {
 const loginCliente = (req, res) => {
     const {email, senha} = req.body;
     if (!email || !senha) {
-        return res.status(400).send("Preencha Todos os Campos!");
+        return res.status(400).send({message: 'Preencha Todos os Campos!'});
     }
 
     pool.query(queries.getClienteByEmail, [email], (error, results) => {
-            if (!results.rows.length) return res.status(404).send('Cliente não existe');
+            if (!results.rows.length) return res.status(404).send({message: 'Cliente não existe'});
             let user = results.rows[0];
             const hash = bcrypt.compareSync(senha, user.senha)
-            if (!user || !hash) return res.status(401).send("Email ou Senha não é válida");
+            if (!user || !hash) return res.status(401).send({message: 'Email ou Senha não é válida'});
             user = {
                 username: results.rows[0].nome,
                 email: results.rows[0].email,
@@ -126,31 +126,31 @@ const loginCliente = (req, res) => {
             const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN, {expiresIn: '1d'});
 
             pool.query(queries.updateRefreshToken, [refreshToken, email], (error, results) => {
-                if (error) return res.status(500).send("Erro ao adicionar refresh token");
+                if (error) return res.status(500).send({message: 'Erro ao adicionar refresh token'});
                 return res.status(200).cookie('refreshToken', refreshToken, {
                     httpOnly: true,
                     secure: true,
                     sameSite: 'Strict',
-                }).json({ token: accessToken});
+                }).json({token: accessToken});
             });
         }
     );
 }
 
 const logoutCliente = (req, res) => {
-    const email = req.user.email;
+    const {email} = req.user;
     pool.query(queries.updateRefreshToken, [null, email], (err, results) => {
-        if(err) return res.status(500).send("Erro ao logout cliente");
-        return res.status(200).clearCookie('refreshToken').send("Cliente fez log-off");
+        if (err) return res.status(500).send({message: 'Erro ao logout cliente'});
+        return res.status(200).clearCookie('refreshToken').send({message: 'Cliente fez log-off'});
     });
 }
 
 const refreshLogin = (req, res) => {
     const {refreshToken} = req.cookies;
-    if (!refreshToken) return res.status(401).send({message: "Token não existe"});
+    if (!refreshToken) return res.status(401).send({message: 'Token não existe'});
 
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, user) => {
-        if (err) return res.status(403).send({message: "Token não válido"});
+        if (err) return res.status(403).send({message: 'Token não válido'});
         delete user.iat;
         delete user.exp;
         const accessToken = criaAccessToken(user);
@@ -161,12 +161,12 @@ const refreshLogin = (req, res) => {
 const validateToken = (req, res) => {
     let token;
     let authHeader = req.headers.Authorization || req.headers.authorization;
-    if (authHeader && authHeader.startsWith("Bearer")) {
-        token = authHeader.split(" ")[1];
-        if (!token) return res.status(401).send("Cliente não autorizado ou token está vazio");
+    if (authHeader && authHeader.startsWith('Bearer')) {
+        token = authHeader.split(' ')[1];
+        if (!token) return res.status(401).send({message: 'Cliente não autorizado ou token está vazio'});
         jwt.verify(token, process.env.ACCESS_TOKEN, (err) => {
             if (err) {
-                return res.status(401).send({message: "Cliente não autorizado"});
+                return res.status(401).send({message: 'Cliente não autorizado'});
             }
             return res.status(200).send({message: 'Token autorizado'});
         });
@@ -174,7 +174,7 @@ const validateToken = (req, res) => {
 }
 
 const criaAccessToken = (user) => {
-    return jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: "20m"})
+    return jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '20m'})
 }
 
 module.exports = {
