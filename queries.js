6e40a3030be8c0049bc0aaecc52630689c847511
Cleiciane Silva <92116369+cleiciane55@users.noreply.github.com>
2024-11-15 20:28:38 -0300

    Ajeitando os textos das queries

--- queries.js
+++ queries.js
@@ -1,9 +1,10 @@
-const addCliente = "INSERT INTO Cliente (nome, email, senha, telefone, endereco) VALUES ($1, $2, $3, $4, $5)";
-const getAllClientes = "SELECT * FROM Cliente";
-const getClienteById = "SELECT * FROM Cliente WHERE id = $1";
-const getClienteByEmail = "SELECT * FROM Cliente WHERE email = $1";
-const checkEmailExiste = "SELECT email FROM Cliente WHERE email = $1"
-const updateRefreshToken = "UPDATE Cliente SET refreshToken = $1 WHERE email = $2";
+const addCliente = "INSERT INTO Cliente (nome, email, senha, telefone, endereco) VALUES ($1, $2, $3, $4, $5);";
+const getAllClientes = "SELECT * FROM Cliente;";
+const getClienteById = "SELECT * FROM Cliente WHERE id = $1;";
+const getClienteByEmail = "SELECT * FROM Cliente WHERE email = $1;";
+const getClienteByToken = "SELECT * FROM Cliente WHERE refreshToken = $1;";
+const checkEmailExiste = "SELECT email FROM Cliente WHERE email = $1;"
+const updateRefreshToken = "UPDATE Cliente SET refreshToken = $1 WHERE email = $2;";
 
 module.exports = {
     addCliente,
@@ -11,5 +12,6 @@ module.exports = {
     getAllClientes,
     getClienteById,
     getClienteByEmail,
+    getClienteByToken,
     checkEmailExiste,
 };
